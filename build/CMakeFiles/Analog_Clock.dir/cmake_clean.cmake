b177f3a262401679cd62c293d3fe647e4871f7a4
l0st-day <lumendany@mail.ru>
2024-05-02 22:30:54 +0300

    Added main.cpp

---
+++ build/CMakeFiles/Analog_Clock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Analog_Clock"
+  "Analog_Clock.pdb"
+  "CMakeFiles/Analog_Clock.dir/src/main.cpp.o"
+  "CMakeFiles/Analog_Clock.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Analog_Clock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Analog_Clock.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Analog_Clock.
+# This may be replaced when dependencies are built.
